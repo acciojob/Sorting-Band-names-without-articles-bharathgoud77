c24a0852ba5a111cde386ef47b06cfed6b623ded
bharathgoud77 <92040880+bharathgoud77@users.noreply.github.com>
2023-03-25 15:04:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main.html'));
+});
 //your code here
-let bandNames = ['The Beatles', 'Pink Floyd', 'Led Zeppelin', 'The Who', 'Queen', 'The Rolling Stones', 'Aerosmith'];
-
-// define an array of articles that we want to exclude while sorting
-const articles = ['a', 'an', 'the'];
-
-// custom sorting function to compare two strings without articles
-function compareStrings(str1, str2) {
-  // split the strings into words and remove articles from them
-  const words1 = str1.split(' ').filter(word => !articles.includes(word.toLowerCase()));
-  const words2 = str2.split(' ').filter(word => !articles.includes(word.toLowerCase()));
-
-  // compare the first non-article words of the two strings
-  const result = words1[0].localeCompare(words2[0]);
-
-  // if the first words are the same, compare the second words and so on
-  return result === 0 ? words1[1]?.localeCompare(words2[1]) ?? 0 : result;
-}
-
-// sort the band names using the custom compare function
-bandNames.sort(compareStrings);
-
-// get the ul element with id 'band'
-const bandList = document.getElementById('band');
-
-// iterate over the sorted band names and add them to the ul element as li elements
-for (let i = 0; i < bandNames.length; i++) {
-  const li = document.createElement('li');
-  li.textContent = bandNames[i];
-  bandList.appendChild(li);
-}
-
+app.post('/add', (req, res) => {
+  const {a,b} = req.body;
+  res.status(200).send(a+b);
+  // res.sendFile(path.join(__dirname + '/main.html'));
+});
+module.exports = app;
